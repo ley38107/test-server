f6af02d542368af2cc20c3c4740f64af5f21c56a
“bu-hl” <banjang.body@gmail.com>
2025-01-18 09:24:48 +0900

    feat: add docker file and update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express')
+    res.send('Hi, Express')
 })
 
 app.listen(app.get('port'), () => {
